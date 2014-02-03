a12c23e623011fa4f75559d0124129f7741e7179
Rob Allen <rob@akrabat.com>
2014-02-03 21:11:40 +0000

    New Apigility skeleton (728a203634)

---
+++ config/application.config.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2013 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+return array(
+    // This should be an array of module namespaces used in the application.
+    'modules' => array(
+        'Application',
+        'ZF\Apigility',
+        'ZF\Apigility\Provider',
+        'ZF\Apigility\Welcome',
+        'AssetManager',
+        'ZF\ApiProblem',
+        'ZF\MvcAuth',
+        'ZF\OAuth2',
+        'ZF\Hal',
+        'ZF\ContentNegotiation',
+        'ZF\ContentValidation',
+        'ZF\Rest',
+        'ZF\Rpc',
+        'ZF\Versioning',
+    ),
+    // This should be an array of paths in which modules reside.
+    // If a string key is provided, the listener will consider that a module
+    // namespace, the value of that key the specific path to that module's
+    // Module class.
+    'module_listener_options' => array(
+        'module_paths' => array(
+            './module',
+            './vendor'
+        ),
+        'config_glob_paths' => array('config/autoload/{,*.}{global,local}.php')
+    )
+);
